abe668e516b850a2f95b5c4f8d8ce95ebfa34a32
ankit <sahuankit.murhu@gmail.com>
2024-09-07 00:40:15 +0530

    Qr code generation

--- src/app/teacher/attendance/page.tsx
+++ src/app/teacher/attendance/page.tsx
@@ -1,26 +1,26 @@
 "use client";
 
-import generateCode from '@/api/generate-code'
-import { Button } from '@/components/ui/button'
-import React, { useState } from 'react'
-
+import generateCode from "@/api/generate-code";
+import { Button } from "@/components/ui/button";
+import React, { useState } from "react";
+import {QRCodeSVG} from 'qrcode.react';
 
 function Attendace() {
-  const [code, setCode] = useState('');
+  const [code, setCode] = useState("");
 
   const onClickHandler = () => {
-    setCode(generateCode())
-  }
+    setCode(generateCode());
+  };
 
   return (
-    <div>
-      <div>
-
-      <Button onClick={onClickHandler}>Click me</Button>
-      <p>{code}</p>
+    <div className="h-screen w-full">
+      <div className="flex flex-col gap-10 h-full justify-center items-center">
+        <Button variant={"secondary"} onClick={onClickHandler}>Generate Code</Button>
+        <QRCodeSVG value='{code}' bgColor="#000" fgColor="#fff" size={250} />
+        <h2 className="text-2xl font-bold">{code}</h2>
       </div>
     </div>
-  )
+  );
 }
 
-export default Attendace
+export default Attendace;
